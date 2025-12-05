c2febfba42d6b61871a224b0b79ecc98f26b3f61
Punam807 <ppunamrani7@gmail.com>
2025-12-05 20:47:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function addTask(){
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+	if (inputBox.value === ''){
+		alert("You must write somthing!");
+	}else{
+		let li = document.createElement("li");
+		li.textContent = inputBox.value;
+		listContainer.appendChild(li);
+
+		let span = document.createElement("span");
+		span.textContent = "\u00D7";
+		li.appendChild(span);
+	}
+	inputBox.value = "";
+}
+const listContainer = document.getElementById("list-container");
+listContainer.addEventListener("click",function(e){
+	if(e.target.tagName === "LI"){
+
+		e.target.classList.toggle("checked");
+	}else if(e.target.tagName === "SPAN"){
+		e.target.parentElement.remove();
+	}
+},false);
